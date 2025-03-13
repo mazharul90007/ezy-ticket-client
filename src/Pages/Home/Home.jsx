977e179fd8ed89aa9c47ab7730f6bd97f30aa234
MahdiHassanGo <contact.mahdiasif@gmail.com>
2025-03-13 11:33:50 +0600

    on homepage 4th button added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,13 @@
-
-
 const Home = () => {
-    return (
-        <div>
-            <h2 className="text-3xl font-semibold">This is Home Page</h2>
-            <button className="btn btn-primary">Click me</button>
-            <button className="btn btn-secondary">Go to Login</button>
-            <button className="btn btn-secondary">Go to Login</button>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-3xl font-semibold">This is Home Page</h2>
+      <button className="btn btn-primary">Click me</button>
+      <button className="btn btn-secondary">Go to Login</button>
+      <button className="btn btn-secondary">Go to HomePage</button>
+      <button className="btn btn-secondary">Go to New Button</button>
+    </div>
+  );
 };
 
 export default Home;
